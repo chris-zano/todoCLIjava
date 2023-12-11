9d70c3c64cf9bfd186f676192ca0d0eb07290837
Christian Solomon <cncs.chris@gmail.com>
2023-12-11 13:02:03 +0000

    delete docs

--- docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"com.example.java","l":"Add"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.java","l":"DeleteTask"},{"p":"com.example.java","l":"Main"},{"p":"com.example.java","l":"Task_List"}];updateSearchResults();
